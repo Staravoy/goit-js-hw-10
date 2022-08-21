994364bec92b6792120f36271c6f5abd566c1b78
Starovoy <staravoy@outlook.com>
2022-08-21 18:19:35 +0300

    home task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+//-----------------------V A R I A B L E S----------------------//
 import './css/styles.css';
-
+const inputCountry = document.querySelector('#search-box');
 const DEBOUNCE_DELAY = 300;
+//-----------------------F U N C T I O N S----------------------//
+inputCountry.addEventListener('input', _.debounce((event) => {
+    event.preventDefault();
+    fetchCountries(inputCountry.value).then(name => console.log(name));
+  }, DEBOUNCE_DELAY)
+);
+function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`).then(response => response.json());
+}
